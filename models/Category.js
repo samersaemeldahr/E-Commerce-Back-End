3486fb16b41a951818a406c617e20574c97985a5
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-08 00:07:32 +0300

    Add info to category model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,14 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: DataTypes.STRING,
+    allowNull: false,
   },
   {
     sequelize,
