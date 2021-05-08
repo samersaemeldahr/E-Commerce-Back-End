a68a7cb937075d40e1a67476ee4b9ae31ef7e4cc
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-08 04:02:44 +0300

    Add relationships to models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "product-category"
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product-category"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tags, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: "product-tags"
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: "product-tags"
+  }
+})
+
 
 module.exports = {
   Product,
