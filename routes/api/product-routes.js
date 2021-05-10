92d335b7a3f9f4ffcbc76ba5b10d67aaaec15ca0
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-10 05:08:32 +0300

    Create route to find a single product by id and its related category and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [{model: Category}, {model: Tag}]
+    });
     res.status(200).json(productData)
   } catch (err) {
     res.status(500).json(err)
@@ -16,9 +18,23 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+     include: [{ model: Category }, {model: Tag}]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
